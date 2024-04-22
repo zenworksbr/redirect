f1f4475f17cdc9bbf4e44fcd539d0c156c5e5b09
r47orr <razoroller529@gmail.com>
2024-04-22 19:18:55 -0300

    novo ip do TTT

--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
 const servers = {
         "ttt": {
                 "name": "NotZenTown",
-                "ip": "138.199.5.224",
+                "ip": "138.199.5.195",
                 "port": 27015
         },
         "zm": {
